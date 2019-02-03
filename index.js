91f66936f18452e160ef245fd171a413254acbc5
neolao <contact@neolao.com>
2019-02-03 23:00:34 +0000

    Add BTC and LTC

---
+++ index.js
@@ -0,0 +1,22 @@
+module.exports = {
+  btc: {
+    bip32: {
+      public: 0x0488b21e,
+      private: 0x0488ade4
+    },
+    bip44: 0,
+    pubKeyHash: 0x00,
+    scriptHash: 0x05,
+    wif: 0x80,
+  },
+  ltc: {
+    bip32: {
+      public: 0x019da462,
+      private: 0x019d9cfe,
+    },
+    bip44: 2,
+    pubKeyHash: 0x30,
+    scriptHash: 0x32,
+    wif: 0xb0,
+  },
+};
